69298842c8e0abac316b2fb3cb6853d39791f78e
vmfernandez4 <vmfernandez4@miners.utep.edu>
2018-12-07 16:03:58 -0700

    refills list after going to all places

--- Runner.py
+++ Runner.py
@@ -40,15 +40,44 @@ class Runner:
 
         random.shuffle(self.non_visited)
 
+    def refill_list(self, visited):
+
+        count = 0
+        fname = []
+
+        with open("List.txt", "w") as file:
+            for i in visited:
+                if count == len(visited) - 1:
+                    file.write(i)
+                else:
+                    file.write(i + ",")
+                count += 1
+
+        with open("visited.txt", "w") as file:
+            file.write("tonteria ")
+
+        self.visited = []
+
+        with open("List.txt") as restaurant_list:
+            for line in restaurant_list:
+                fname = line.rstrip().split(',')
+
+
+        self.stacker(fname, self.non_visited)
+
+
     def get_place(self):
+        if not self.non_visited:
+            self.refill_list(self.visited)
 
         self.shuffler()
-
         tmp = self.non_visited.pop()
         self.list_writer("List.txt", self.non_visited)
         self.visited.append(tmp)
         self.list_writer("visited.txt", self.visited)
 
+        if tmp == "nishi ramen":
+            return "F"
         return tmp
 
     def list_writer(self, filename, place):
