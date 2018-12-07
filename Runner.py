b319db43cd56ad155ccffc5342a8661c4efe1772
vmfernandez4 <42975177+vmfernandez4@users.noreply.github.com>
2018-12-07 10:58:35 -0700

    added methods

---
+++ Runner.py
@@ -0,0 +1,22 @@
+import random
+
+def file_reader():
+
+    return 0
+
+def stacker():
+
+    return 0
+
+def shuffler():
+
+    return 0
+
+def places():
+
+    return 0
+
+def visited_list():
+
+    return 0
+
